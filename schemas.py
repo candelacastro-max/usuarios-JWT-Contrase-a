413caff88eb6bcce3e987121b1c17619b135740e
Candela Castro <candelacastro@iresm.edu.ar>
2026-06-09 13:09:00 -0300

    xd

---
+++ schemas.py
@@ -0,0 +1,48 @@
+from pydantic import BaseModel, EmailStr
+
+
+
+class ProductoCreate(BaseModel):
+ nombre:str
+ precio:float
+ en_stock:bool
+ categoria_id:int
+
+class ProductoResponse (ProductoCreate):
+ id: int
+ class Config:
+     orn_mode = True
+
+class CategoriaBase(BaseModel):
+  nombre:str
+class CategoriaCreate(CategoriaBase):
+  pass
+
+class CategoriaResponse(CategoriaBase):
+  id: int
+
+  class Config:
+    orm_mode = True
+
+
+#Usuario
+
+
+class UsuarioBase (BaseModel):
+ nombre: str
+ email: EmailStr
+
+class UsuarioCreate (UsuarioBase):
+ password: str
+ es_admin: bool = False
+
+class UsuarioResponse (UsuarioBase):
+ id: int
+ es_admin: bool
+
+class Config:
+ orm_node = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
